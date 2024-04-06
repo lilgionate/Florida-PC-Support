08d50380c27497b7ca389931d53c51d1afe87ccc
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-04-06 16:13:06 -0500

    navbar updates

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,48 +1,58 @@
-import React, { Component } from "react";
-import "./navbar.scss";
-import MenuItems from "./MenuItems";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./navbar.scss";
+import { navItems } from "./NavItems";
+import Button from "./Button";
+import Dropdown from "./Dropdown";
 
-class Navbar extends Component {
-  state = { clicked: false };
+function Navbar() {
+  const [dropdown, setDropdown] = useState(false);
+  const [clicked, setClicked] = useState(false); 
 
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+  const handleClick = () => {
+    setClicked(!clicked);
   };
 
-  render() {
-    return (
-      <nav className="navbarItems">
-        <h1 className="navbar-logo">
-          <a href="http://localhost:5173/">Florida PC Support</a>
-        </h1>
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="http://localhost:5173/" className="navbar-logo" >
+          <img src={logo} alt="" />
+        </Link>
 
-        <div className="menu-icons" onClick={this.handleClick}>
-          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        <div className="menu-icons" onClick={handleClick}>
+          <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => (
-            <li key={index}>
-              <Link className={item.cName} to={item.url}>
-                {item.title}
-              </Link>
-              {item.submenu && (
-                <ul className="dropdown-menu" style={{ display: this.state.clicked ? "block" : "none" }}>
-                  {item.submenu.map((subItem, subIndex) => (
-                    <li key={subIndex}>
-                      <Link to={subItem.url}>{subItem.title}</Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-          <button>Shop Now</button>
+        
+        <div className="navbar-container">
+        <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+          {navItems.map((item) => {
+            if (item.title === "Services") {
+              return (
+                <li
+                  key={item.id}
+                  className={item.cName}
+                  onMouseEnter={() => setDropdown(true)}
+                  onMouseLeave={() => setDropdown(false)}
+                >
+                  <Link to={item.path}>{item.title}</Link>
+                  {dropdown && <Dropdown />}
+                </li>
+              );
+            }
+            return (
+              <li key={item.id} className={item.cName}>
+                <Link className='nav-links' to={item.path}>{item.title}</Link>
+              </li>
+            );
+          })}
         </ul>
+        <Button />
+        </div>
       </nav>
-    );
-  }
+    </>
+  );
 }
 
 export default Navbar;
