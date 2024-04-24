159274bff0647708603cb62756d12341d7d0f604
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-04-24 02:02:22 -0500

    data pricing complete

--- src/components/DataRecovery/PricingCard.jsx
+++ src/components/DataRecovery/PricingCard.jsx
@@ -8,8 +8,11 @@ const PricingCard = ({ title, services }) => {
                 {services.map((service, index) => (
                     <><div key={index} className="sectionDataClasses">
                         <h3 className="priceDataClasses">{service.name}</h3>
-                        <span>{service.price}</span>
-                    </div><p>{service.description}</p></>
+                        {[...Array(service.dots)].map((_, dotIndex) => (
+                                <span key={dotIndex} className="dot">.</span>
+                            ))}
+                        <span className="data-pricing">{service.price}</span>
+                    </div><p className="data-price-des">{service.description}</p></>
                 ))}
             </div>
         </div>
