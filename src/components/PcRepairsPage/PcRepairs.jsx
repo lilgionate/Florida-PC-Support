2aef90908f7247312f7d6b0ed349f94a33b56d5e
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-05-06 01:19:21 -0500

    added content to pc page

--- src/components/PcRepairsPage/PcRepairs.jsx
+++ src/components/PcRepairsPage/PcRepairs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './PcRepairs.scss';
 
 import MacBookCenter from '../../assets/pc-service-center.jpg';
+import CommonIssuesList from './SupportedPcIssues';
 
 
 const PcRepairPage = () => {
@@ -24,7 +25,15 @@ const PcRepairPage = () => {
             </div>
         </div>
 
-        
+        <CommonIssuesList />
+
+        <div className="pc-repair-service-container">
+            <h1 className="pc-repair-service-heading">Drop-off or Onsite Windows / Microsoft repair service: You choose</h1>
+            <p className="pc-repair-service-paragraph">
+                Whether you prefer a free diagnostic first or same-day visit, we will accommodate you. We will work around your schedule. You may schedule for one of our Florida Microsoft repair service experts to come to your home or office during the same day. Or you may drop off at our repair centers. Either way, you will get a certified Windows PC repair expert take care of your Microsoft issue.
+            </p>
+        </div>
+
         </>
 )}
 
